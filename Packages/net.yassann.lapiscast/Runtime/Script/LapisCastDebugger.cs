782493df6cd5921e2a0fdd1d7b4bdf4eb81c7196
yassann325 <yassann456@gmail.com>
2025-05-09 19:35:05 +0900

    LapisCastDebuggerの機能を追加 テストイベントのパラメータを設定できるように変更 パラメータの同期を実装、テストイベントは全員が送るようにした UI.Textへの書き出し機能は削除

--- Packages/net.yassann.lapiscast/Runtime/Script/LapisCastDebugger.cs
+++ Packages/net.yassann.lapiscast/Runtime/Script/LapisCastDebugger.cs
@@ -3,26 +3,62 @@ using UdonSharp;
 using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine;
+using VRC.SDK3.Components;
 using LapisCast;
 using VRC.SDK3.Data;
 using UnityEngine.UI;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class LapisCastDebugger : LapisCastBehaviour
 {
-    public Text LogText = null;
-    public bool SendDebugEvent = true;
+    [SerializeField, UdonSynced]
+    public bool sendDebugEvent = true;
+    [SerializeField]
+    private Toggle debugModeToggle;
+
+    [Space(20)]
+    [SerializeField]
+    private VRCUrlInputField EventNameInputField;
+    [SerializeField]
+    private Text EventNameText;
+    [UdonSynced]
+    public string EventName = "debugEvent";
+
+    [Space(20)]
+    [SerializeField]
+    private VRCUrlInputField ValueInputField;
+    [SerializeField]
+    private Text ValueText;
+    [UdonSynced]
+    public string Value = "testValue";
+
+    [Space(20)]
+    [SerializeField]
+    private VRCUrlInputField SpaceNameInputField;
+    [SerializeField]
+    private Text SpaceNameText;
+    [UdonSynced]
+    public string SpacetName = "debugSpace";
+
+    [Space(20)]
+    [UdonSynced]
+    private float eventSendIntervalTime = 1;
     private float timer = 0;
+
     void Start()
     {
-        LapisCastBehaviourInit("UdonDebugger");
-        if(LogText){
-            LogText.text = "";
+        if(SpacetName == ""){
+            SpacetName = "debugSpace";
         }
+        LapisCastBehaviourInit(SpacetName);
+        EventNameText.text = EventName;
+        ValueText.text = Value;
+        SpaceNameText.text = SpacetName;
     }
 
     private void Update() {
-        if(SendDebugEvent){
-            if(timer > 1){
+        if(sendDebugEvent && Networking.IsMaster){
+            if(timer > eventSendIntervalTime){
                 timer = 0;
                 SendTestEvent();
             }
@@ -30,26 +66,67 @@ public class LapisCastDebugger : LapisCastBehaviour
         }
     }
 
+    public override void OnDeserialization()
+    {
+        debugModeToggle.isOn = sendDebugEvent;
+
+        EventNameText.text = EventName;
+        ValueText.text = Value;
+        SpaceNameText.text = SpacetName;
+        SetSpaceName(SpacetName);
+    }
+
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if(Networking.IsOwner(gameObject)){
+            RequestSerialization();
+        }
+    }
+
+    // Set SendDebugEvent State
+    public void ToggleSendDebugEvent(){
+        SetSendDebugEventState(!sendDebugEvent);
+    }
+    public void EnableSendDebugEvent(){
+        SetSendDebugEventState(true);
+    }
+    public void DisableSendDebugEvent(){
+        SetSendDebugEventState(false);
+    }
+    public void SetSendDebugEventState(bool state){
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        sendDebugEvent = state;
+        RequestSerialization();
+    }
 
     //Send Event
     public void SendTestEvent(){
-        SendLapisCast("DebugerEvent", new DataToken("Debuger Hello!"));
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "doTestEvent");
     }
 
-    //Receive All Events
-    public override void OnLapisCastAllEvent(string spanename, string keyname, DataToken value, bool sameinstance)
-    {
-        if(LogText){
-            LogText.text += $"{spanename} | {keyname}/{value}  sameinstance {sameinstance}\n";
-        }
-        else{
-            Debug.Log($"{spanename} | {keyname}/{value}  sameinstance {sameinstance}");
-        }
+    public void doTestEvent(){
+        SendLapisCast(EventName, new DataToken(Value));
     }
 
-    //Receive UdonDebugger Event
-    public override void OnLapisCastEvent(string spanename, string keyname, DataToken value, bool sameinstance)
-    {
-        Debug.Log($"{spanename} | {keyname}/{value}  sameinstance {sameinstance}");
+    public void OnEventNameChanged(){
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        EventName = EventNameInputField.GetUrl().ToString();
+        RequestSerialization();
+    }
+
+    public void OnValueChanged(){
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        Value = ValueInputField.GetUrl().ToString();
+        RequestSerialization();
+    }
+
+    public void OnSpaceNameChanged(){
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        SpacetName = SpaceNameInputField.GetUrl().ToString();
+        if(SpacetName == ""){
+            SpacetName = "debugSpace";
+        }
+        RequestSerialization();
     }
 }
