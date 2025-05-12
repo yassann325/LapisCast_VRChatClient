a212d33e32a5c2c594cd544c8d72450d513b4581
yassann325 <yassann456@gmail.com>
2025-05-12 13:48:27 +0900

    GetUnixOffset()をGetUnixSceneStartTime()に変更 時刻補正を徐々に行うように変更 GetUnixTimestamp()を追加

--- Packages/net.yassann.lapiscast/Runtime/Script/LapisCastTimelineClock.cs
+++ Packages/net.yassann.lapiscast/Runtime/Script/LapisCastTimelineClock.cs
@@ -9,17 +9,18 @@ using VRC.SDK3.Data;
 namespace LapisCast{
     public class LapisCastTimelineClock : UdonSharpBehaviour
     {
-        private double initUnixOffset = 0;
+        private double targetAdjustUnixOffset = 0;
+        private double adjustmentUnixOffset = 0;
         private DataList unixOffsetList = new DataList();
 
         void Start()
         {
-            initUnixOffset = GetLocalHostUnixTime();
+            adjustmentUnixOffset = GetLocalHostUnixTime();
         }
 
         void Update()
         {
-            
+            adjustmentUnixOffset = MoveTowardsDouble(adjustmentUnixOffset, targetAdjustUnixOffset, Time.deltaTime*0.5f);
         }
 
         //getHostTimestamp
@@ -33,27 +34,46 @@ namespace LapisCast{
             return Math.Round(elapsedTime.TotalSeconds, 3);
         }
 
-        public double GetUnixOffset(){
-            double offsetTime = initUnixOffset;
-            if(unixOffsetList.Count > 0){
-                offsetTime = 0;
-                for(int i = 0; i < unixOffsetList.Count; i++){
-                    offsetTime += unixOffsetList[i].Double;
-                }
-                offsetTime /= unixOffsetList.Count;
-            }
-            return offsetTime;
+        //======================================================//
+        // LapisCast Clock
+        // return UnixTime when HostBased SceneStartTime or ServerBased SceneStartTime
+        public double GetUnixSceneStartTime(){
+            return adjustmentUnixOffset;
         }
 
+        // Adjust SceneStartTime use ServerBased Timestamp
         public void AdjustTimelineClock(double serverTime, double offsetTime){
             unixOffsetList.Add(new DataToken(serverTime + offsetTime - Time.time));
             if(unixOffsetList.Count > 10){
                 unixOffsetList.RemoveAt(0);
             }
+
+            double aveTime = 0;
+            for(int i = 0; i < unixOffsetList.Count; i++){
+                aveTime += unixOffsetList[i].Double;
+            }
+            aveTime /= unixOffsetList.Count;
+
+            targetAdjustUnixOffset = aveTime;
+            if(Math.Abs(targetAdjustUnixOffset - adjustmentUnixOffset) > 3){
+                adjustmentUnixOffset = targetAdjustUnixOffset;
+            }
+        }
+
+        public double GetUnixTimestamp(){
+            return GetUnixSceneStartTime() + Time.time;
+        }
+
+        private double MoveTowardsDouble(double current, double target, double maxDelta)
+        {
+            double difference = target - current;
+            if (Math.Abs(difference) <= maxDelta)
+                return target;
+            return current + Math.Sign(difference) * maxDelta;
         }
 
         public double GetTimestamp(){
-            return GetLocalHostUnixTime() + Time.time;
+            return GetUnixTimestamp();
         }
     }
 }
