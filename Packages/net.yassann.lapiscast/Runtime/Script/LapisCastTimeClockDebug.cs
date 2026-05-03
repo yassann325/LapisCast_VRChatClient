fcf6a113e14789d602d946bf4300bdf4af74c239
yassann325 <yassann325@gmail.com>
2026-05-03 12:49:19 +0900

    Lapis内の時刻情報を確認するためのプレハブを作成

---
+++ Packages/net.yassann.lapiscast/Runtime/Script/LapisCastTimeClockDebug.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using LapisCast;
+using TMPro;
+
+public class LapisCastTimeClockDebug : LapisCastBehaviour
+{
+    [SerializeField]
+    private TMP_Text text;
+
+    void Start()
+    {
+        LapisCastBehaviourInit();
+    }
+
+    void Update()
+    {
+        if(!text){ return; }
+
+        string [] debugTexts = {
+        /* LapisCast Time */    GetLapisCastCore().GetTimestamp().ToString("F1"),
+        /* LocalHostTime */     GetLapisCastCore().GetLocalHostUnixTime().ToString("F1"),
+        /* UnixTime */          GetLapisCastCore().GetUnixTimestamp().ToString("F1"),
+        /* StreamTime */        GetLapisCastCore().GetStreamTimestamp().ToString("F1"),
+        /* Unix - Stream */     (GetLapisCastCore().GetUnixTimestamp() - GetLapisCastCore().GetStreamTimestamp()).ToString("F3")
+        };
+        text.text = string.Join("\n", debugTexts);       
+    }
+}
